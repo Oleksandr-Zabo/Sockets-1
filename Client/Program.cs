163974fa69ced18a0c10d872a2fd4e4226a020ba
Oleksandr-Zabo <zoleksandr06@gmail.com>
2025-05-06 20:30:45 +0300

    master

---
+++ Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Client;
+
+class Program
+{
+    static void Main()
+    {
+        IPAddress ip = IPAddress.Parse("192.168.0.10");
+        IPEndPoint ep = new IPEndPoint(ip, 80);
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+            s.Connect(ep);
+            if (s.Connected)
+            {
+                string strSend = "GET / HTTP/1.1\r\nHost: 192.168.0.10\r\nConnection: close\r\n\r\n";
+                s.Send(System.Text.Encoding.ASCII.GetBytes(strSend));
+                byte[] buffer = new byte[1024];
+                int l;
+                do
+                {
+                    l = s.Receive(buffer);
+                    Console.WriteLine(System.Text.Encoding.ASCII.GetString(buffer, 0, l));
+                } while (l > 0);
+            }
+            else
+            {
+                Console.WriteLine("Not connected");
+            }
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine($"Error client: {ex.Message}");
+        }
+        finally
+        {
+            if (s.Connected)
+            {
+                s.Shutdown(SocketShutdown.Both);
+            }
+            s.Close();
+        }
+    }
+}
---
+++ MainServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MainServer;
+
+class Program
+{
+    static void Main()
+    {
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+        IPAddress ip = IPAddress.Parse("192.168.0.10");
+        IPEndPoint ep = new IPEndPoint(ip, 80);
+        s.Bind(ep);
+        s.Listen(10);
+
+        try
+        {
+            while (true)
+            {
+                Socket ns = s.Accept();
+                Console.WriteLine(ns.RemoteEndPoint?.ToString());
+                ns.Send(System.Text.Encoding.Unicode.GetBytes(DateTime.Now.ToString(CultureInfo.InvariantCulture)));
+                ns.Shutdown(SocketShutdown.Both);
+                ns.Close();
+            }
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine($"Error server: {ex.Message}");
+        }
+    }
+}
